e1dd9d4b45bc4939959ca517a0560beede4e108f
fatemajalal18 <fatema.w2000@gmail.com>
2018-06-19 19:58:14 +0000

    Done.

---
+++ while.rb
@@ -0,0 +1,9 @@
+def using_while
+  puts "Wingardium Leviosa"
+  levitation_force= 6 
+  while levitation_force <10
+  levitation_force +=1 
+  puts "levitation_force=#{levitation_force}"
+end 
+end 
+using_while
