1bdca3799cba5a836b300b24df091a121adaf33b
miriamdonovan <miriamdonovan@gmail.com>
2018-06-20 18:51:36 +0000

    Done.

---
+++ whileanduntilloops.rb
@@ -0,0 +1,11 @@
+def force 
+  force = 6
+  while force < 10 
+    puts "Wingardium Leviosa"
+    force += 1 
+    puts "we have #{force} levitation "
+  end
+end
+
+force
+
---
+++ whileanduntilloops2.rb
@@ -0,0 +1,10 @@
+def force
+  force = 6
+  until force == 10 
+   puts "Wingardium Leviosa"
+    force +=1 
+    puts "we have #{force} levitation"
+  end
+end
+
+force
